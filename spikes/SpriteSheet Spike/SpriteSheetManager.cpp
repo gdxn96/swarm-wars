b82dfdfa27d4e93b7f70f32b73a42b930fcd9bb5
Lanc3 <aaron.keating.lanc3@gmail.com>
2015-10-09 12:40:44 +0100

    finished spriteSheet animator

--- spikes/SpriteSheet Spike/SpriteSheetManager.cpp
+++ spikes/SpriteSheet Spike/SpriteSheetManager.cpp
@@ -2,11 +2,14 @@
 #include "SpriteSheetManager.h"
 #include <iostream>
 using namespace std;
+
+
 SpriteSheetManager::SpriteSheetManager()
 {
 }
-SpriteSheetManager::SpriteSheetManager(const sf::Texture & _texture,int _columnAmount,int _rowAmount,Vector2D _position)
+SpriteSheetManager::SpriteSheetManager(const sf::Texture & _texture,int _columnAmount,int _rowAmount,Vector2D &_position, Vector2D _scale)
 {
+	debug = false;
 	texture = _texture;
 	width = texture.getSize().x;
 	height = texture.getSize().y;
@@ -17,7 +20,10 @@ SpriteSheetManager::SpriteSheetManager(const sf::Texture & _texture,int _columnA
 	columnAmount = _columnAmount;
 	rowAmount = _rowAmount;
 	position = _position;
-	
+	isLooping = false;
+	isDead = false;
+	willDie = false;
+	cellOrigin = Vector2D(cellWidth/2,cellHeight/2);
 	for(int i = 0; i < rowAmount;i ++)
 	{
 		animation temp;
@@ -30,40 +36,55 @@ SpriteSheetManager::SpriteSheetManager(const sf::Texture & _texture,int _columnA
 	}
 	texture.setSmooth(true);
 	sf::Sprite t(texture);
-	//outputSprite.setOrigin(256, 256);
+	outputSprite.setOrigin(cellOrigin.toSFMLVector());
 	outputSprite.setTexture(texture,true);
 	outputSprite.setPosition(position.toSFMLVector());
-	outputSprite.setScale(.5f,0.5f);
+	outputSprite.setScale(_scale.toSFMLVector());
 }
+
 void SpriteSheetManager::Update()
 {
-	
+
 	sf::Time elapsedTime = clock.getElapsedTime();
 	if(elapsedTime > sf::seconds(0.1f))
 	{
 		if(selectedColumn < columnAmount-1)
 		{
 			selectedColumn++;
-			
+
 		}
 		else
 		{
-			selectedColumn = 0;
+			if(isLooping || willDie == false)
+			{
+				selectedColumn = 0;
+			}
+			if(willDie)
+			{
+				isDead = true;
+			}
 		}
-		cout << "col "<<selectedColumn<< endl;
-		cout << "row "<<selectedRow<< endl;
+		if(debug)
+		{
+			cout << "col "<<selectedColumn<< endl;
+			cout << "row "<<selectedRow<< endl;
+		}
 		clock.restart();
 	}
-	
+
+	outputSprite.setRotation(rotation);
+	outputSprite.setScale(scale.toSFMLVector());
 	outputSprite.setTextureRect(animationList[selectedRow].RectangleList[selectedColumn]);
 	outputSprite.setPosition(position.toSFMLVector());
 }
-void SpriteSheetManager::setPosition(Vector2D _position)
+
+void SpriteSheetManager::setPosition(Vector2D & _position)
 {
 	position = _position;
 }
 void SpriteSheetManager::Draw(sf::RenderWindow & window)
 {
+	if(!isDead)
 	window.draw(outputSprite);
 }
 void SpriteSheetManager::setRunningAnimation(std::string name)
@@ -80,10 +101,6 @@ void SpriteSheetManager::setRunningAnimation(std::string name)
 		}
 	}
 }
-sf::Sprite SpriteSheetManager::getOutputImage()
-{
-	return outputSprite;
-}
 void SpriteSheetManager::setRotation(float _rotate)
 {
 	rotation = _rotate;
@@ -94,6 +111,18 @@ void SpriteSheetManager::setAnimationNameToIndex(int index,std::string name)
 	temp.index = index;
 	temp.name = name;
 	animationIndixList.push_back(temp);
+}
+void SpriteSheetManager::setScale(Vector2D _scale)
+{
+	scale = _scale;
+}
+void SpriteSheetManager::setIsLooping(bool _isLooping)
+{
+	isLooping = _isLooping;
+}
+bool SpriteSheetManager::getIfDead()
+{
+	return isDead;
 }
 SpriteSheetManager::~SpriteSheetManager(void)
 {

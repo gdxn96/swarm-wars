a16479ab9747ff41141efd4608f34a6d0fb9c828
Lanc3 <aaron.keating.lanc3@gmail.com>
2015-11-04 09:40:18 +0000

    fixed issues

--- Spikes/lighting/LightManager.h
+++ Spikes/lighting/LightManager.h
@@ -4,15 +4,16 @@
 class LightManager
 {
 public:
-	LightManager(void);
+	LightManager(sf::Vector2f,sf::Vector2f,sf::Color);
 	~LightManager(void);
-	void Update(sf::RenderWindow & ,sf::Vector2f);
-	void AddLight(Light);
+	void Update(sf::RenderWindow &,sf::Vector2f);
+	void AddLight(sf::Vector2f,sf::Vector2f,  sf::Color,sf::Vector2f);
 private:
+	std::vector<Light> lights;
+	sf::Texture lightTexture;
+	sf::Sprite light;
 	sf::RenderTexture lightMapTexture;
 	sf::Sprite lightmap;
-	sf::Sprite loadedSprite;
-	sf::Texture lightTexture;
-	std::vector<Light> lights;
+	sf::Sprite getLightMap();
 };
 
